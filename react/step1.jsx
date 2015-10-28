dfe3b76df1e941adf378abfd7a64be4db7573f90
Neel Mehta <neelmehta@college.harvard.edu>
2015-10-28 16:07:13 -0400

    Your first component (step 1)

---
+++ react/step1.jsx
@@ -0,0 +1,16 @@
+/**
+ * Step 1: your first component
+ */
+
+let App = React.createClass({
+  render: function() {
+    return (
+      <div>
+          <p>Question: What is the ultimate answer to life, the universe, and everything?</p>
+          <p>Answer: 42</p>
+      </div>
+    );
+  }
+});
+
+React.render(<App />, document.getElementById('page'));
